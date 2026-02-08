7878ce16467d9909d39cc11f44c2eea15282e5c7
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-02-08 09:41:38 +0100

    v0.0.5

--- src/parser.c
+++ src/parser.c
@@ -1018,7 +1018,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_dotenv(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 0,
-      .patch_version = 4,
+      .patch_version = 5,
     },
   };
   return &language;
