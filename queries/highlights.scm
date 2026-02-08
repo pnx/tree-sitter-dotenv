d403ce05d8429df6a902eb3197f67b022b8e6274
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-02-08 09:37:29 +0100

    queries/highlights.scm: format

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,7 +1,16 @@
 "=" @operator
+
 (boolean) @constant
+
 (number) @number
+
 (string) @string
-[(identifier) (variable)] @variable
+
+[
+  (identifier)
+  (variable)
+] @variable
+
 (value) @string
+
 (comment) @comment
