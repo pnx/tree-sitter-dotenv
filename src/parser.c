c0570dec1d2f99b81d7c38d3fc8d76323a6abfc9
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2026-07-15 09:06:27 +0200

    chore: version v1.0.1

--- src/parser.c
+++ src/parser.c
@@ -1329,7 +1329,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_dotenv(void) {
     .metadata = {
       .major_version = 1,
       .minor_version = 0,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
